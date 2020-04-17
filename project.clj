28a98e770b54ab0052d83062ff6cbdb054e42daa
rmangi <rick@chartbeat.com>
2020-04-17 14:49:18 -0400

    add license

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
-(defproject com.chartbeat.cljbeat/ottla "1.0-SNAPSHOT"
+(defproject com.chartbeat.cljbeat/ottla "0.9-SNAPSHOT"
   :description "An opinionated framework for writing kafka consumers, producers and consumer-producers."
+  :url "https://github.com/chartbeat-labs/ottla"
+  :license {:name "Apache License"
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/tools.cli "1.0.194"]
                  [org.clojure/tools.logging "1.0.0"]
                  [clojurewerkz/propertied "1.3.0"]
@@ -8,5 +11,4 @@
                  [org.apache.kafka/kafka-clients "2.4.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "1.0.0"]]
                    :source-paths ["dev" "test"]}}
-
   :vcs :git)
